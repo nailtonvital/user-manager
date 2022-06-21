009a0bd985b035246fc4f6f02d96ecf03410e1ea
Nailton Vital <nailtonvital35@gmail.com>
2022-06-21 18:29:46 -0300

    date and adjusts

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -11,10 +11,60 @@ class UserController{
 
         this.formEl.addEventListener("submit", (e)=>{
             e.preventDefault();
+
+            let btn = this.formEl.querySelector("[type=submit]")
+
+            btn.disabled = true
+
+            let values = this.getValues();
+
+            this.getPhoto()
+            .then((content)=>{
+                values.photo = content;
+
+                this.addline(values)
+
+                this.formEl.reset();
+
+                btn.disabled = true;
+
+            }, (e)=>{
+                console.log(e)
+            })
+
+
         
-            this.addline(this.getValues())
         });
 
+    }
+
+    getPhoto(callback){
+
+        return new Promise((resolve,reject)=>{
+
+            let fileReader = new FileReader();
+
+            let elements = [...this.formEl.elements].filter(item=>{
+                if (item.name === "photo"){
+                    return item;
+                }
+            })
+
+            let file = elements[0].files[0];
+
+            fileReader.onload = ()=>{
+                resolve(fileReader.result)
+            }
+
+            fileReader.onerror = (e)=>{
+                reject(e)
+            }
+
+            fileReader.readAsDataURL(file);
+
+        })
+
+        
     }
 
     getValues(){
@@ -26,6 +76,8 @@ class UserController{
                 if(field.checked){
                     user[field.name] = field.value;
                 }
+            } else if(field.name == "admin"){
+                user[field.name] = field.checked;
             } else{
                 user[field.name] = field.value;
             }
@@ -41,28 +93,26 @@ class UserController{
             user.photo,
             user.admin
             );
-
-        
     }
 
     addline(dataUser, tableId){
 
-        console.log(dataUser)
+        let tr = document.createElement('tr')
     
-        this.tableId.innerHTML = `
+        tr.innerHTML = `
             <tr>
                 <td><img src="dist/img/user1-128x128.jpg" alt="User Image" class="img-circle img-sm"></td>
                 <td>${dataUser.name}</td>
                 <td>${dataUser.email}</td>
-                <td>${dataUser.admin}</td>
-                <td>${dataUser.birth}</td>
+                <td>${dataUser.admin ? "Sim" : "Não"}</td>
+                <td>${dataUser.register}</td>
                 <td>
                 <button type="button" class="btn btn-primary btn-xs btn-flat">Editar</button>
                 <button type="button" class="btn btn-danger btn-xs btn-flat">Excluir</button>
                 </td>
             </tr>
         `
-    
-    }
 
+        this.tableId.appendChild(tr)
+    }
 }
